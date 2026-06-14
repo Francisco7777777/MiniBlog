9fd06921c35a0e6c0b286812447b707eacb3e9dd
Francisco7777777 <frajoseom@gmail.com>
2026-06-14 16:01:00 -0300

    Criando as primeiras telas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 
+//import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
 function App() {
   return (
-    <>
-      <h1>Mine Blog</h1>
-    </>
+    <BrowserRouter>
+      <div className="conteiner">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
