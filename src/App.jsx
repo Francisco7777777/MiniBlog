e1f4e75cb8298456df8cfb2a6f39177f59db7833
Francisco7777777 <frajoseom@gmail.com>
2026-06-14 19:17:20 -0300

    Adição de estilos

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <div className="conteiner">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Escreva sobre o que você trm interesse!</h3>
+      <p>Mini blog</p>
+    </footer>
+  );
+};
+
+export default Footer;
